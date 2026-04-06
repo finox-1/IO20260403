c30e92f3ece5f573c60d77bb38f01e18f549d4f4
enkhboldzu <enkhboldzu@gmail.com>
2026-04-06 19:12:07 +0800

    11

---
+++ app/api/router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+const todoDemoRouter = require("./todoDemo");
+
+router.use("/demo", todoDemoRouter);
+
+router.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../../dist", "index.html"));
+});
+
+module.exports = router;
