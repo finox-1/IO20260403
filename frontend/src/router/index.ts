494f45cd1334e9d1fef3c060e1e36d5bf7a0693e
Amarmurun <Amarmurun11.com>
2026-04-07 10:17:27 +0800

    C25IO1068: Updated app.js, added /card route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router' //npm install vue-router@4
 import LoginView from '../components/login.vue' 
 import RegisterView from '../components/Register.vue'
+import Card from "../CardApp.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +20,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/card',
+      name: 'card',
+      component: Card
     }
   ]
 })
